5e3250fac47e7c643fb029ff3290d9e10e64d7d4
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-10-18 15:32:36 -0300

    Skeleton

---
+++ src/entity/customer.ts
@@ -0,0 +1,45 @@
+import Address from "./address";
+
+export default class Customer {
+
+    _id: string;
+    _name: string = "";
+    _address!: Address;
+    _active: boolean = false;
+
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;
+        this.validate();
+    }
+
+    validate(): void {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._name.length === 0) {
+            throw new Error("Id is required");
+        }
+    }
+
+    changeName(name: string): void {
+        this._name = name;
+        this.validate();
+    }
+
+    activate(): void {
+        if (this._address === undefined) {
+            throw new Error("Address is mandatory to activate customer");
+        }
+        this._active = true;
+    }
+
+    deactivate(): void {
+        this._active = false;
+    }
+
+    set address(address: Address) {
+        this._address = address;
+    }
+    
+}
