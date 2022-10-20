7375bf952cf69317386478460c59f907a2fcd29c
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-10-20 14:23:52 -0300

    Adjust test script runner to apply tsc for all files

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua projetada", 2, "85.440-000", "Ubiratã");
 customer.address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "item 1", 10);
-const item2 = new OrderItem("2", "item 2", 15);
+const item1 = new OrderItem("1", "1", "item 1", 10, 1);
+const item2 = new OrderItem("2", "2", "item 2", 15, 2);
 const order = new Order("1", "123", [item1, item2]);
