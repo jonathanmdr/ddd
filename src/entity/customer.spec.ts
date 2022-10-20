3ad10fbe7fd0efdfe099b63f21ef375f4cb267c0
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-10-20 09:49:11 -0300

    Adjusts customer entity and added new unit test cases

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,17 +1,71 @@
+import Address from "./address";
 import Customer from "./customer";
 
-describe("Customer unit tests", () => {
+describe("Customer unit tests", (): void => {
 
-    it("should thrown error when id is empty", () => {
-        expect(() => {
+    it("should thrown error when id is empty", (): void => {
+        expect((): void => {
             new Customer("", "John Doe");
         }).toThrowError("Id is required");
     });
 
-    it("should thrown error when name is empty", () => {
-        expect(() => {
+    it("should thrown error when name is empty", (): void => {
+        expect((): void => {
             new Customer("123", "");
         }).toThrowError("Name is required");
+    });
+
+    it("should thrown error when change name to empty", (): void => {
+        const customer = new Customer("123", "John");
+
+        expect((): void => {
+            customer.changeName("");
+        }).toThrowError("Name is required");
+        expect(customer.name).toBe("John");
+    });
+
+    it("should change name", (): void => {
+        const customer = new Customer("123", "John");
+        customer.changeName("John Doe");
+
+        expect(customer.name).toBe("John Doe");
+    });
+
+    it("should thrown error when activate customer without address", (): void => {
+        const customer = new Customer("123", "John");
+
+        expect((): void => {
+            customer.activate();
+        }).toThrowError("Address is mandatory to activate customer");
+        expect(customer.isActive()).toBe(false);
+    });
+
+    it("should activate customer", (): void => {
+        const customer = new Customer("123", "John");
+        const address = new Address("Street One", 123, "98.765-432", "São Paulo");
+        customer.address = address;
+
+        expect(customer.isActive()).toBe(false);
+
+        customer.activate();
+
+        expect(customer.isActive()).toBe(true);
+    });
+
+    it("should deactivate customer", (): void => {
+        const customer = new Customer("123", "John");
+        const address = new Address("Street One", 123, "98.765-432", "São Paulo");
+        customer.address = address;
+
+        expect(customer.isActive()).toBe(false);
+
+        customer.activate();
+
+        expect(customer.isActive()).toBe(true);
+
+        customer.deactivate();
+
+        expect(customer.isActive()).toBe(false);
     });
 
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -8,27 +8,35 @@ export default class Customer {
     private _active: boolean = false;
 
     constructor(id: string, name: string) {
+        this.validate(id, name);
+
         this._id = id;
         this._name = name;
-        this.validate();
     }
 
-    private validate(): void {
-        if (this._id.length === 0) {
+    private validate(id: string, name: string): void {
+        if (this.isInvalidString(id)) {
             throw new Error("Id is required");
         }
-        if (this._name.length === 0) {
+        if (this.isInvalidString(name)) {
             throw new Error("Name is required");
         }
     }
 
+    private isInvalidString(value: string): boolean {
+        return typeof value === undefined
+            || value === null
+            || value.length === 0;
+    }
+
     public changeName(name: string): void {
+        this.validate(this._id, name);
+
         this._name = name;
-        this.validate();
     }
 
     public activate(): void {
-        if (this._address === undefined) {
+        if (this._address === undefined || this._address === null) {
             throw new Error("Address is mandatory to activate customer");
         }
         this._active = true;
@@ -38,8 +46,16 @@ export default class Customer {
         this._active = false;
     }
 
-    set address(address: Address) {
+    public isActive(): boolean {
+        return this._active;
+    }
+
+    public set address(address: Address) {
         this._address = address;
+    }
+
+    public get name(): string {
+        return this._name;
     }
     
 }
